742e82af40ead586275d544dece20936320e55ff
JRInge <JRInge@users.noreply.github.com>
2014-08-04 23:16:54 +0100

    Bump version to 1.2.0

--- GeocacheQRCode/Geocache_QR_Code.meta.js
+++ GeocacheQRCode/Geocache_QR_Code.meta.js
@@ -4,13 +4,13 @@
 // @description Allows you to display a QR code linking to a geocache or trackable listing on geocaching.com
 // @include     http://www.geocaching.com/geocache/GC*
 // @include     https://www.geocaching.com/geocache/GC*
+// @include     https://www.geocaching.com/map*
 // @include     http://www.geocaching.com/track/details.aspx*
 // @include     https://www.geocaching.com/track/details.aspx*
-// @author      JRI
 // @oujs:author JRI
 // @license     MIT License; http://www.opensource.org/licenses/mit-license.php
 // @copyright   2011-14, James Inge (http://geo.inge.org.uk/)
-// @version     1.1.1
+// @version     1.2.0
 // @icon        http://geo.inge.org.uk/userscripts/QRicon48.png
 // @icon64      http://geo.inge.org.uk/userscripts/QRicon64.png
 // @grant       none
