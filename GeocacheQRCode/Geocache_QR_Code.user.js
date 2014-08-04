342441e37714bcfc1eb92d03fbec77115bac663c
JRInge <JRInge@users.noreply.github.com>
2014-08-04 23:02:48 +0100

    Added QR codes to map page
    Adds link to display QR code into popup template for cache details at
    geocaching.com/map

--- GeocacheQRCode/Geocache_QR_Code.user.js
+++ GeocacheQRCode/Geocache_QR_Code.user.js
@@ -4,13 +4,13 @@
 // @description Allows you to display a QR code linking to a geocache or trackable listing on geocaching.com
 // @include     http://www.geocaching.com/geocache/GC*
 // @include     https://www.geocaching.com/geocache/GC*
+// @include     https://www.geocaching.com/map*
 // @include     http://www.geocaching.com/track/details.aspx*
 // @include     https://www.geocaching.com/track/details.aspx*
-// @author      JRI
 // @oujs:author JRI
 // @license     MIT License; http://www.opensource.org/licenses/mit-license.php
 // @copyright   2011-14, James Inge (http://geo.inge.org.uk/)
-// @version     1.1.1
+// @version     1.2.0
 // @icon        http://geo.inge.org.uk/userscripts/QRicon48.png
 // @icon64      http://geo.inge.org.uk/userscripts/QRicon64.png
 // @grant       none
@@ -18,6 +18,7 @@
 // @downloadURL https://openuserjs.org/install/JRI/Geocache_QR_Code.user.js
 // ==/UserScript==
 //
+// v1.2.0 - Added QR codes to cache details on map page
 // v1.1.1 - Updated metadata for OpenUserJS.org
 // v1.1.0 - Added QR codes for trackables
 // v1.0.4 - updated for changes to geocaching.com cache page URLS.
@@ -28,6 +29,7 @@
   "use strict";
   var target = document.getElementById('dlgClipboard'),
     source = document.getElementById('ctl00_ContentBody_CoordInfoLinkControl1_uxCoordInfoCode'),
+    template = document.getElementById('cacheDetailsTemplate'),
     i = document.createElement("img");
 
   // Don't run on frames or iframes
@@ -40,8 +42,12 @@
     i.alt = "QR Code";
     target.appendChild(i);
     target.style.height = "205px";
-    console.info("Geocache QR Code v1.1.1");
+    console.info("Geocache QR Code v1.2.0");
   } else {
-    console.error("Geocache QR Code v1.1.1 didn't understand page structure.");
+    if (template) {
+      template.textContent = template.textContent.replace(/<div class=\"links Clear\">/, '<div class="links Clear"> <a style="padding-left: 1em; margin-right: 0.5em; background: -436px -1px url(http://www.geocaching.com/map/css/themes/images/icons-18-black.png);" onclick="$.fancybox(\'<img width=\\\\\\\'186px\\\\\\\' src=\\\\\\\'http://qrcode.kaywa.com/img.php?s=6&d=http://coord.info/{{=gc}}\\\\\\\'>\');">QR</a> ');
+    } else {
+      console.error("Geocache QR Code v1.2.0 didn't understand page structure.");
+    }
   }
 }());
