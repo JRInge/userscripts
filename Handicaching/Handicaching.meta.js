fcc36162206b6489e1caace1465ff492c9798b82
JRInge <JRInge@users.noreply.github.com>
2014-10-27 22:08:47 +0000

    Handicaching - initial load to github (v0.0.6)
    Updates over v0.0.5: shows Handicaching info on new log confirmation
    page, to prompt cache rating. Moved icon to https version on Github.

---
+++ Handicaching/Handicaching.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Handicaching
+// @namespace   inge.org.uk/userscripts
+// @description Makes geocaching more accessible
+// @match       *://www.geocaching.com/geocache/GC*
+// @match       *://www.geocaching.com/seek/log.aspx*
+// @match       http://www.handicaching.com/rate.php*
+// @match       http://www.handicaching.com/show.php*
+// @version     0.0.6
+// @oujs:author JRI
+// @license     MIT License; http://www.opensource.org/licenses/mit-license.php
+// @copyright   2011-14, James Inge (http://geo.inge.org.uk/)
+// @icon        https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheQRCode/Handicaching/handicaching_icon.png
+// @grant       GM_xmlhttpRequest
+// @updateURL   http://geo.inge.org.uk/userscripts/handicaching.meta.js
+// @downloadURL https://openuserjs.org/install/JRI/Handicaching.user.js
+// ==/UserScript==
