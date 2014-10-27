fdb95b3e008659cb0cb33e2a896eee91fc73b437
JRInge <JRInge@users.noreply.github.com>
2014-10-27 22:16:13 +0000

    Corrected typo in icon URL.

--- Handicaching/Handicaching.meta.js
+++ Handicaching/Handicaching.meta.js
@@ -10,7 +10,7 @@
 // @oujs:author JRI
 // @license     MIT License; http://www.opensource.org/licenses/mit-license.php
 // @copyright   2011-14, James Inge (http://geo.inge.org.uk/)
-// @icon        https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheQRCode/Handicaching/handicaching_icon.png
+// @icon        https://raw.githubusercontent.com/JRInge/userscripts/master/Handicaching/handicaching_icon.png
 // @grant       GM_xmlhttpRequest
 // @updateURL   http://geo.inge.org.uk/userscripts/handicaching.meta.js
 // @downloadURL https://openuserjs.org/install/JRI/Handicaching.user.js
