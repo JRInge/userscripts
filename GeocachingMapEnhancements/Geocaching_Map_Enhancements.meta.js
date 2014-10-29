7f77c5aaf879124b74f227c834b78f0b0c025979
JRInge <JRInge@users.noreply.github.com>
2014-10-29 23:07:47 +0000

    Update GME to v0.7.1
    + Added GME functionality to cache hiding maps.
    * Changed icons, images and default map URLs to https to avoid mixed
    (insecure) content.
    * Fixed Chrome 38 bug on Mac.
    * Minor bugfixes and tidying.
    - Removed CloudMade map source.

---
+++ GeocachingMapEnhancements/Geocaching_Map_Enhancements.meta.js
@@ -0,0 +1,20 @@
+﻿// ==UserScript==
+// @name				Geocaching Map Enhancements
+// @version			0.7.1
+// @author			JRI
+// @oujs:author JRI
+// @namespace		inge.org.uk/userscripts
+// @description Adds Ordnance Survey maps and grid reference search to Geocaching.com, along with several other enhancements.
+// @include			http://www.geocaching.com/*
+// @include			https://www.geocaching.com/*
+// @license			MIT License; http://www.opensource.org/licenses/mit-license.php
+// @copyright		2011-14, James Inge (http://geo.inge.org.uk/)
+// @attribution GeoNames (http://www.geonames.org/)
+// @attribution Chris Veness (http://www.movable-type.co.uk/scripts/latlong-gridref.html)
+// @grant				GM_xmlhttpRequest
+// @grant				GM_log
+// @icon				https://raw.githubusercontent.com/JRInge/userscripts/master/GeocachingMapEnhancements/GeocachingMap48.png
+// @icon64			https://raw.githubusercontent.com/JRInge/userscripts/master/GeocachingMapEnhancements/GeocachingMap64.png
+// @updateURL		http://geo.inge.org.uk/userscripts/GeocachingMapEnhancements.meta.js
+// @downloadURL https://openuserjs.org/install/JRI/Geocaching_Map_Enhancements.user.js
+// ==/UserScript==
