b96e2b94a79f565f56ec712ad4ecabaa5674dd01
JRInge <JRInge@users.noreply.github.com>
2014-11-20 23:33:35 +0000

    Metadata update
    Add attribution, icons, download links, etc.

--- GCFloodAlert/GC_Flood_Alerts.meta.js
+++ GCFloodAlert/GC_Flood_Alerts.meta.js
@@ -1,9 +1,18 @@
 // ==UserScript==
 // @name        GC Flood Alerts
+// @version     0.1.1
+// @author      JRI
+// @oujs:author JRI
 // @namespace   inge.org.uk/userscripts
-// @description Show flood alerts on the Geocaching.com map
+// @description Show flood alerts for England and Wales on the Geocaching.com map
+// @license     MIT License; http://www.opensource.org/licenses/mit-license.php
+// @copyright   2014, James Inge (http://geo.inge.org.uk/)
+// @attribution Environment Agency (http://apps.environment-agency.gov.uk/flood/142151.aspx)
 // @include     https://www.geocaching.com/map/*
 // @exclude     https://www.geocaching.com/map/MapPreferences.aspx*
-// @version     0.1.0
 // @grant       none
+// @icon        https://raw.githubusercontent.com/JRInge/userscripts/master/GCFloodAlert/GC_Flood_Alert_Icon48.png
+// @icon64      https://raw.githubusercontent.com/JRInge/userscripts/master/GCFloodAlert/GC_Flood_Alert_Icon64.png
+// @updateURL   http://geo.inge.org.uk/userscripts/GC_Flood_Alerts.meta.js
+// @downloadURL https://openuserjs.org/install/JRI/GC_Flood_Alerts.user.js
 // ==/UserScript==
