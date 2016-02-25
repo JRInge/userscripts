bfbc07b96b9979fca6205c11769a308321834695
JRInge <JRInge@users.noreply.github.com>
2016-02-25 00:01:48 +0000

    Fix icon names

--- GeocacheCircles/src/Geocache_Circles.meta.js
+++ GeocacheCircles/src/Geocache_Circles.meta.js
@@ -7,8 +7,8 @@
 // @license     MIT License; http://www.opensource.org/licenses/mit-license.php
 // @copyright   2016, James Inge (http://geo.inge.org.uk/)
 // @version     0.0.1
-// @icon        https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheCircles/QRicon48.png
-// @icon64      https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheCircles/QRicon64.png
+// @icon        https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheCircles/circle48.png
+// @icon64      https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheCircles/circle64.png
 // @grant       GM_xmlhttpRequest
 // @updateURL   http://geo.inge.org.uk/userscripts/Geocache_Circles.meta.js
 // @downloadURL https://openuserjs.org/install/JRI/Geocache_Circles.user.js
