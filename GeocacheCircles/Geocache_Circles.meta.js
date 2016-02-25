bd919aed6e8d6a8491bb4d764da54adc6edbf84b
JRInge <JRInge@users.noreply.github.com>
2016-02-25 00:10:07 +0000

    Rebuilt with fixed metadata

--- GeocacheCircles/Geocache_Circles.meta.js
+++ GeocacheCircles/Geocache_Circles.meta.js
@@ -3,12 +3,13 @@
 // @namespace   inge.org.uk/userscripts
 // @description Allows you to display a 0.1 mile radius circle around a cache on the Geocaching.com map page
 // @include     https://www.geocaching.com/map*
+// @author      JRI
 // @oujs:author JRI
 // @license     MIT License; http://www.opensource.org/licenses/mit-license.php
 // @copyright   2016, James Inge (http://geo.inge.org.uk/)
 // @version     0.0.1
-// @icon        https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheCircles/QRicon48.png
-// @icon64      https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheCircles/QRicon64.png
+// @icon        https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheCircles/circle48.png
+// @icon64      https://raw.githubusercontent.com/JRInge/userscripts/master/GeocacheCircles/circle64.png
 // @grant       GM_xmlhttpRequest
 // @updateURL   http://geo.inge.org.uk/userscripts/Geocache_Circles.meta.js
 // @downloadURL https://openuserjs.org/install/JRI/Geocache_Circles.user.js
