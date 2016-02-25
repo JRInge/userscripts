d66ae80f73d7e0d0e98af7d13b88b65d93c8f5eb
JRInge <JRInge@users.noreply.github.com>
2016-02-25 00:04:50 +0000

    Add author metadata
    For Tampermonkey

--- GeocacheCircles/src/Geocache_Circles.meta.js
+++ GeocacheCircles/src/Geocache_Circles.meta.js
@@ -3,6 +3,7 @@
 // @namespace   inge.org.uk/userscripts
 // @description Allows you to display a 0.1 mile radius circle around a cache on the Geocaching.com map page
 // @include     https://www.geocaching.com/map*
+// @author      JRI
 // @oujs:author JRI
 // @license     MIT License; http://www.opensource.org/licenses/mit-license.php
 // @copyright   2016, James Inge (http://geo.inge.org.uk/)
