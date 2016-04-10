0c19be26bab53adae04dfb5d5aa0ea4c05f7ca30
JRInge <JRInge@users.noreply.github.com>
2016-04-10 17:26:25 +0100

    Include version in log messages.

--- GeocacheHeight/src/Geocache_Height.js
+++ GeocacheHeight/src/Geocache_Height.js
@@ -51,6 +51,7 @@
 
     var coords = getCoords("ctl00_ContentBody_uxViewLargerMap");
     var target = document.getElementById("uxLatLon");
+    var scriptId = "Geocache Height v1.1.0 ";
 
     //don't run on frames or iframes
     if (window.top !== window.self) {
@@ -58,19 +59,21 @@
     }
 
     if (isPMOnly()) {
-        console.warn("Geocache Height run on Premium Member cache - coordinates not available");
+        console.warn(scriptId + "run on Premium Member cache - coordinates not available");
         return;
     }
 
     if (target === null) {
-        console.error("Geocache Height couldn't find where to display height on the cache page");
+        console.error(scriptId + "couldn't find where to display height on the cache page");
         return;
     }
 
     if (coords === null) {
-        console.error("Geocache Height couldn't work out coordinates for cache");
+        console.error(scriptId + "couldn't work out coordinates for cache");
         return;
     }
+
+    console.info(scriptId);
 
     GM_xmlhttpRequest({
         method: 'GET',
