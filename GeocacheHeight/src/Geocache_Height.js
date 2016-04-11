c4b5a3a74065bba9a909cbdec77c2cc5cce18c3c
JRInge <JRInge@users.noreply.github.com>
2016-04-11 23:17:37 +0100

    Factored out formatHeight
    To allow for new version in feet rather than metres.

--- GeocacheHeight/src/Geocache_Height.js
+++ GeocacheHeight/src/Geocache_Height.js
@@ -15,14 +15,6 @@
         return null;
     }
 
-    function formatHeight(height) {
-        var heightElement = document.createElement("span");
-        heightElement.id = "jriCacheHeight";
-        heightElement.innerHTML = (height >= 0) ? " +" : " ";
-        heightElement.innerHTML += Math.round(height) + "m";
-        return heightElement;
-    }
-    
     function isPMOnly() {
         var form = document.getElementById("aspnetForm");
         if (form && /cache_pmo\.aspx/.test(form.action)) {
---
+++ GeocacheHeight/src/formatHeight.js
@@ -0,0 +1,8 @@
+    function formatHeight(height) {
+        var heightElement = document.createElement("span");
+        heightElement.id = "jriCacheHeight";
+        heightElement.innerHTML = (height >= 0) ? " +" : " ";
+        heightElement.innerHTML += Math.round(height) + "m";
+        return heightElement;
+    }
+    
