295b8c71ec44783b9be213bdf83e9f34cfa563fb
JRInge <JRInge@users.noreply.github.com>
2017-11-19 23:44:08 +0000

    Update metadata
    Match standalone metadata block to userscript

--- GeocachingMapEnhancements/Geocaching_Map_Enhancements.meta.js
+++ GeocachingMapEnhancements/Geocaching_Map_Enhancements.meta.js
@@ -6,19 +6,19 @@
 // @namespace   inge.org.uk/userscripts
 // @description Adds extra maps and grid reference search to Geocaching.com, along with several other enhancements.
 // @include     https://www.geocaching.com/*
-// @license     MIT License; http://www.opensource.org/licenses/mit-license.php
+// @license     MIT; http://www.opensource.org/licenses/mit-license.php
 // @copyright   2011-17, James Inge (http://geo.inge.org.uk/)
 // @attribution GeoNames (http://www.geonames.org/)
 // @attribution Postcodes.io (https://postcodes.io/)
 // @attribution Chris Veness (http://www.movable-type.co.uk/scripts/latlong-gridref.html)
 // @grant       GM_xmlhttpRequest
+// @grant       GM.xmlHttpRequest
 // @connect     geograph.org.uk
 // @connect     channel-islands.geographs.org
 // @connect     geo-en.hlipp.de
 // @connect     api.geonames.org
 // @connect     api.postcodes.io
 // @connect     www.geocaching.com
-// @grant       GM_log
 // @icon        https://geo.inge.org.uk/userscripts/GeocachingMap48.png
 // @icon64      https://geo.inge.org.uk/userscripts/GeocachingMap64.png
 // @updateURL   https://geo.inge.org.uk/userscripts/GeocachingMapEnhancements.meta.js
