2052fcadffc24766473086d3038973b406f11ddb
JRInge <JRInge@users.noreply.github.com>
2017-12-25 23:25:10 +0000

    New linting settings
    Use ES6, and stricter checks in JSHint.
    Remove unnecessary globals.

--- GeocacheCircles/src/header.js
+++ GeocacheCircles/src/header.js
@@ -1,6 +1,7 @@
 
-/*jslint browser*/
-/*global CustomEvent, GM_xmlhttpRequest, L,  MapSettings, window, console */
+/*jslint browser:true */
+/*jshint esversion: 6, undef:true, unused:true */
+/*global L, MapSettings, window, console */
 
 (function () {
     "use strict";
